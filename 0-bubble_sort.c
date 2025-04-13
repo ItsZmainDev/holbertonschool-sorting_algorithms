7c45fd03b44581e84cd7089c2a712b4e8254f639
ItsZmainDev <zmainpro@gmail.com>
2025-04-13 21:29:33 +0200

    Correct the checker

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,27 +9,24 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp;
+	int temp;
 	int swapped;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
-	{
+	do {
 		swapped = 0;
-		for (j = 0; j < size - 1 - i; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
 				swapped = 1;
 				print_array(array, size);
 			}
 		}
-		if (!swapped)
-			break;
-	}
+	} while (swapped);
 }
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,39 +1,40 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
-* insertion_sort_list - Sorts a doubly linked list using insertion sort
-* @list: Pointer to the head pointer of the list
-*/
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: Double pointer to the head of the doubly linked list
+ */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *temp;
 
-	if (!list || !*list || !(*list)->next)
+	if (list == NULL || *list == NULL)
 		return;
 
 	current = (*list)->next;
 
-	while (current)
+	while (current != NULL)
 	{
 		temp = current;
 		current = current->next;
 
-		while (temp->prev && temp->n < temp->prev->n)
+		while (temp->prev != NULL && temp->n < temp->prev->n)
 		{
-			/* Swap nodes */
 			temp->prev->next = temp->next;
-			if (temp->next)
+			if (temp->next != NULL)
 				temp->next->prev = temp->prev;
 
 			temp->next = temp->prev;
 			temp->prev = temp->prev->prev;
-			temp->next->prev = temp;
 
-			if (temp->prev)
+			if (temp->prev != NULL)
 				temp->prev->next = temp;
 			else
 				*list = temp;
 
+			temp->next->prev = temp;
 			print_list(*list);
 		}
 	}
