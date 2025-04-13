f20dccf65e567943867e5c6d47baf46143b3cd8a
Terencetjt26 <10597@holbertonstudents.com>
2025-04-13 11:45:39 +0000

    implement insertion sort on doubly linked list with step-by-step printing

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+* insertion_sort_list - Sorts a doubly linked list using insertion sort
+* @list: Pointer to the head pointer of the list
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	current = (*list)->next;
+
+	while (current)
+	{
+		temp = current;
+		current = current->next;
+
+		while (temp->prev && temp->n < temp->prev->n)
+		{
+			/* Swap nodes */
+			temp->prev->next = temp->next;
+			if (temp->next)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			if (temp->prev)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+
+			print_list(*list);
+		}
+	}
+}
